68bb06068af921bdc91358974707c3116ea180c1
George <dotnetgeorge@gmail.com>
2013-12-30 17:40:25 +0200

    SRM 144 Div 2

---
+++ SRM144Div2/Time.cs
@@ -0,0 +1,17 @@
+﻿//Problem - http://wp.me/p42BNa-1P
+using System;
+
+class Time
+{
+    static void Main(string[] args)
+    {
+        int seconds = int.Parse(Console.ReadLine());
+        Console.WriteLine(whatTime(seconds));
+    }
+    public static string whatTime(int seconds)
+    {
+        TimeSpan ts = TimeSpan.FromSeconds(seconds);
+        string result = ts.Hours + ":" + ts.Minutes + ":" + ts.Seconds;
+        return result;
+    }
+}
